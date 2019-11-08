82d7beccb4d716f0f7371993c2ecabee406c8f76
Christopher Lyons <ChristopherLyons17@hotmail.com>
2019-11-07 17:13:11 -0700

    initialize MVC App

---
+++ PIXIRunnerApp/PIXIRunnerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PIXIRunnerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
