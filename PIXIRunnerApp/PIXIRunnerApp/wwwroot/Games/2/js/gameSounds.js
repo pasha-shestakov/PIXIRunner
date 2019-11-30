ed6c515973ca47fd2b896e8382037d40bbfe18e9
Christopher Lyons <ChristopherLyons17@hotmail.com>
2019-11-30 15:18:51 -0700

    updated game2 js files

---
+++ PIXIRunnerApp/PIXIRunnerApp/wwwroot/Games/2/js/gameSounds.js
@@ -0,0 +1,196 @@
+﻿export default class GameSounds {
+    constructor() { }
+
+    //only a single bg at one time.
+    bg;
+
+    //for effects
+    playing_sounds = {};
+    soundID = 0;
+
+    disableSound = false;
+    //sound effects
+    soundEffectVolume = 0.10;
+    
+    coin_obj = {
+        sound: new Audio('/Games/1/sounds/coin.wav'),
+        scale: 1
+    }
+    
+    rock_obj = {
+        sound: new Audio('/Games/1/sounds/fall_rock.mp3'),
+        scale: .8
+    }
+
+    throw_obj = {
+        sound: new Audio('/Games/1/sounds/throw.wav'),
+        scale: 1
+    }
+    
+    spike_obj = {
+        sound: new Audio('/Games/1/sounds/spike.wav'),
+        scale: 1
+    }
+
+    walk = {
+        sound: new Audio('/Games/1/sounds/player_walk.wav'),
+        scale: 1
+    }
+
+    oof = {
+        sound: new Audio('/Games/1/sounds/oof.wav'),
+        scale: 1
+    }
+
+    open_menu = {
+        sound: new Audio('/Games/1/sounds/menu_open.wav'),
+        scale: 1
+    }
+
+    chest = {
+        sound: new Audio('/Games/1/sounds/chest_open.wav'),
+        scale: 1
+    }
+
+    lever = {
+        sound: new Audio('/Games/1/sounds/lever.wav'),
+        scale: 1.3
+    }
+
+    open_door_obj = {
+        sound: new Audio('/Games/1/sounds/open_door.wav'),
+        scale: 1
+    }
+
+    //music
+    musicVolume = 0.06;
+    currentIndex;
+    maxIndex = 1;
+    start_bg_music(index) {
+        this.currentIndex = index;
+        this.bg = new Audio('/Games/1/sounds/bg_music' + index + '.mp3');
+
+        if (this.disableSound)
+            this.bg.volume = 0;
+        else
+            this.bg.volume = this.musicVolume;
+            
+        this.bg.load();
+        this.bg.play(); //every call to play is added to the dictionary;
+
+        $(this.bg).on("ended", function () {
+
+            if (this.currentIndex == this.maxIndex)
+                this.currentIndex = 0;
+            else
+                this.currentIndex++;
+
+            this.start_bg_music(this.currentIndex);
+        }.bind(this));
+
+
+        
+        
+    }
+
+    set_volume(type, level) {
+        if (type === 'bg') { //background
+            //will affect all new background music.
+            this.musicVolume = level;
+
+            if (this.bg) this.bg.volume = level;
+
+
+        } else if (type === 'se') { //sound effect
+
+            //will affect all new sound effects.
+            this.soundEffectVolume = level;
+            //TODO
+            for (var id in this.playing_sounds) {
+                this.playing_sounds[id].volume = level;
+            }
+        }
+    }
+
+    toggleEnable() {
+        this.disableSound = !this.disableSound;
+        this.updateSoundEnabled();
+    }
+    updateSoundEnabled() {
+        if (this.disableSound) {
+            if (this.bg) this.bg.volume = 0;
+            for (var id in this.playing_sounds) {
+                this.playing_sounds[id].pause();
+                delete this.playing_sounds[id];
+            }
+            this.playing_sounds = {};
+        }
+        else {
+            if (this.bg) this.bg.volume = this.musicVolume;
+        }
+    }
+    set_enabled(enabled) {
+        this.disableSound = enabled;
+        this.updateSoundEnabled();
+    }
+
+    disable_sounds() {
+        this.disableSound = false;
+    }
+
+    play_sound(obj) {
+        let sound = obj.sound;
+        if (!this.disableSound) {
+            sound.volume = this.soundEffectVolume * obj.scale;
+            sound.load();
+            sound.play();
+
+            let thisID = this.soundID++;
+            this.playing_sounds[thisID] = sound;
+            $(sound).on("ended", function () {
+
+                delete this.playing_sounds[thisID];
+            }.bind(this));
+        }
+        
+    }
+    coin() {
+        this.play_sound(this.coin_obj);
+    }
+
+    rock_hit() {
+        this.play_sound(this.rock_obj);
+    }
+
+    rock_throw() {
+        this.play_sound(this.throw_obj);
+    }
+
+    spike() {
+        this.play_sound(this.spike_obj);
+    }
+
+    player_walk() {
+        this.play_sound(this.walk);
+    }
+
+    player_hurt() {
+        this.play_sound(this.oof);
+    }
+
+    menu_open() {
+        this.play_sound(this.open_menu);
+    }
+
+    open_chest() {
+        this.play_sound(this.chest);
+    }
+
+    pull_lever() {
+        this.play_sound(this.lever);
+    }
+
+    open_door() {
+        this.play_sound(this.open_door_obj);
+    }
+}
--- PIXIRunnerApp/PIXIRunnerApp/wwwroot/Games/2/js/game_controller.js
+++ PIXIRunnerApp/PIXIRunnerApp/wwwroot/Games/2/js/game_controller.js
@@ -1,37 +1,230 @@
 ﻿import { PhysicsGame } from './physics_game.js';
-const game = new PhysicsGame();
+import GameSounds from './gameSounds.js'
+const sounds = new GameSounds();
+const game = new PhysicsGame(sounds);
 game.preInit();
 
-$(document).ready(function () {
-    var saves = document.querySelectorAll("#launcher button");
-    const gameID = document.querySelector(".cont").id;
+var _userSaveState;
+var _userGameState;
+var _userGameSettings;
+var _gameId;
+var _availibleSprites;
 
-    saves.forEach((el) => {
-        el.addEventListener("click", (el) => {
-            if (!el.target.id) {
-                console.log("new button");
-                $.ajax({
-                    url: "/Game/NewGame?id="+ gameID,
-                    success: function (response) {
-                        game.onLoad(response);
-                        game.init();
-                        document.getElementById("launcher").hidden = true;
-                    }
-                });
-                return;
-            }
+var currentSelection = 0;
+
+$(document).ready(function () {
+
+    _gameId = document.querySelector(".cont").id;
+    $('#play').click((el) => {
+        if (currentSelection === 0) {
+            console.log("new button");
             $.ajax({
-                url: "/Game/SavedStates?id=" + el.target.id,
+                url: "/Game/NewGame?id=" + _gameId,
                 success: function (response) {
-                    game.onLoad(response);
-                    game.init();
-                    document.getElementById("launcher").hidden = true;
+                    _userSaveState = new UserSaveState(response);
+
+                    initGameState();
+                }
+            });
+        } else {
+            $.ajax({
+                url: "/Game/SavedStates?id=" + currentSelection,
+                success: function (response) {
+                    _userSaveState = new UserSaveState(response);
+                    initGameState();
+
 
                 }
             });
+        }
 
-        })
+
     });
 
+    initStoreSetup();
+
+    $('.closed_door').mouseover(showSprite);
+    $('.closed_door').mouseout(hideSprite);
+    $('.closed_door').click(clickedDoor);
+
+    $('#closeStoreBtn').click({ name: 'store' }, toggleOverlay);
+    $('#closeSettingsBtn').click({ name: 'settings' }, toggleOverlay);
+
+    $('#disabled').on('input', function () { _userGameSettings.toggleDisabledSound() })
+    $('#musicSlider').on('input', function (value) { _userGameSettings.setMusicVolume(value.target.value) });
+    $('#effectsSlider').on('input', function (value) { _userGameSettings.setEffectVolume(value.target.value) });
+    initGameSettings();
+
+    $("#save_states").change(function () {
+        currentSelection = $("#save_states").val();
+        console.log(currentSelection);
+    });
 });
+
+
+
+function initGameSettings() {
+    $.get("/Game/UserGameSettings?id=" + _gameId, (data) => {
+        if (data) {
+            _userGameSettings = new UserGameSettings(data.id, data.soundDisabled, data.musicVolume, data.soundEffectVolume);
+            sounds.set_volume('se', _userGameSettings.soundEffectVolume);
+            sounds.set_volume('bg', _userGameSettings.musicVolume);
+            sounds.set_enabled(_userGameSettings.soundDisabled);
+            //document.getElementById('disabled').value = _userGameSettings.soundDisabled ? 'off' : 'on';
+
+            document.getElementById('disabled').checked = _userGameSettings.soundDisabled;
+
+            let isEnabled = _userGameSettings.soundDisabled
+            if (isEnabled) {
+                $('#musicSlider').prop('disabled', true);
+                $('#effectsSlider').prop('disabled', true);
+            } else {
+                $('#musicSlider').prop('disabled', false);
+                $('#effectsSlider').prop('disabled', false);
+            }
+            document.getElementById('musicSlider').value = _userGameSettings.musicVolume;
+            document.getElementById('effectsSlider').value = _userGameSettings.soundEffectVolume;
+        }
+    });
+}
+
+function initGameState() {
+    return new Promise((resolve, reject) => {
+
+        $.get("/Game/UserGameState?id=" + _gameId, (data) => {
+            if (data) {
+                _userGameState = new UserGameState(
+                    data.id,
+                    data.gold,
+                    data.unlockedSkins,
+                    data.selectedSkin,
+                    data.minutesPlayed
+                );
+                resolve();
+            }
+        })
+    }).then(() => {
+        game.onLoad(_userSaveState, _userGameState);
+        game.init();
+        document.getElementById("launcher").style.display = "none";
+    });
+
+}
+
+
+window.onbeforeunload = function () {
+    saveGameAutomatically();
+}
+
+function saveGameAutomatically() {
+
+    _userSaveState.updateSaveState();
+    _userGameState.updateGameState();
+}
+
+function toggleOverlay(event) {
+    let name = event.data.name;
+    $('#' + name).toggleClass('show');
+    game.toggle_pause();
+    game.overlayActive = false;
+}
+
+function initStoreSetup() {
+    var store = document.getElementById('store_grid');
+    for (var i = 0; i < 7; i++) {
+        store.innerHTML += "<img class=\"closed_door\" id=\"door" + i + "\" src=\"/Games/1/images/store/closed_door.png\" />";
+    };
+    store.innerHTML += "<img id=\"selected_text\" src=\"/Games/1/images/store/select.png\" />";
+
+    // TODO: db query this
+    store.innerHTML += "<img id=\"selected_sprite\" src=\"/Games/1/images/store/door0.png\" />";
+
+}
+
+function showAquired() {
+
+}
+
+function clickedDoor() {
+    // TODO: error checking for non-existent sprites
+}
+
+function showSprite() {
+    // TODO: error checking for non-existent sprites
+    if ($(this).attr('id').charAt(4) < 3) {
+        $(this).attr('src', '/Games/1/images/store/' + $(this).attr('id') + '.png');
+    }
+}
+
+function hideSprite() {
+    $(this).attr('src', '/Games/1/images/store/closed_door.png');
+}
+
+class UserGameState { // GLOBAL between all games
+    constructor(id, gold, unlockedSkins, selectedSkin, minutesPlayed) {
+        this.id = id;
+        this.gold = gold;
+        this.unlockedSkins = unlockedSkins;
+        this.selectedSkin = selectedSkin;
+        this.minutesPlayed = minutesPlayed;
+    }
+
+    updateGameState() {
+        $.post("/Game/UpdateUserGameState", this);
+    }
+}
+
+class UserSaveState { // SPECIFIC to a SINGLE game
+    constructor(response) {
+        console.log("HELLO: ", response);
+        this.saveStateID = response.saveStateID;
+        this.gameID = response.gameID;
+        this.userID = response.userID;
+        this.checkpoint = response.checkpoint;
+        this.lives = response.lives;
+        this.maxLives = response.maxLives;
+    }
+
+    updateSaveState() {
+        $.post("/Game/UpdateSaveState", this);
+    }
+}
+
+class UserGameSettings {
+    constructor(id, soundDisabled, musicVolume, soundEffectVolume) {
+        this.id = id;
+        this.soundDisabled = soundDisabled;
+        this.musicVolume = musicVolume;
+        this.soundEffectVolume = soundEffectVolume;
+    }
+    toggleDisabledSound() {
+        sounds.toggleEnable();
+        this.soundDisabled = !this.soundDisabled;
+        let isDisabled = $('#musicSlider').is(':disabled');
+        if (isDisabled) $('#musicSlider').prop('disabled', false); else $('#musicSlider').prop('disabled', true);
+
+        isDisabled = $('#effectsSlider').is(':disabled');
+        if (isDisabled) $('#effectsSlider').prop('disabled', false); else $('#effectsSlider').prop('disabled', true);
+        this.updateGameSettings();
+    }
+
+    setMusicVolume(volume) {
+        this.musicVolume = Number(volume);
+        this.updateGameSettings();
+        sounds.set_volume('bg', this.musicVolume);
+    }
+    setEffectVolume(volume) {
+        this.soundEffectVolume = Number(volume);
+        this.updateGameSettings();
+        sounds.set_volume('se', this.soundEffectVolume);
+    }
+
+    /**
+     * updateGameSettings updates the UserGameSettings database record with this
+     */
+    updateGameSettings() {
+        $.post("/Game/UpdateUserGameSettings", this);
+    }
+
+}
     
